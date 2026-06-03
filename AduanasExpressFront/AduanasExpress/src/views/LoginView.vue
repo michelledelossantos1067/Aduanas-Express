242c831a88fcc33c5f4538df39755e3d47ff4b63
Carlos Reynoso <malacacatuana5@gmail.com>
2026-06-03 10:56:40 -0400

    configuracion inicial

---
+++ AduanasExpressFront/AduanasExpress/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const form = ref({
+  email: '',
+  password: ''
+})
+
+const submitForm = async () => {
+  console.log('Login:', form.value)
+  // Cuando el backend esté listo:
+  // const response = await authService.login(form.value)
+  // router.push('/dashboard')
+}
+</script>
+
+<template>
+  <div class="form-container">
+    <h2>Iniciar Sesión</h2>
+
+    <form @submit.prevent="submitForm">
+      <div>
+        <label>Email:</label>
+        <input v-model="form.email" type="email" placeholder="correo@ejemplo.com" required />
+      </div>
+
+      <div>
+        <label>Contraseña:</label>
+        <input v-model="form.password" type="password" placeholderfind src -type f
+="••••••••" required />
+      </div>
+
+      <button type="submit">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form-container { max-width: 400px; margin: auto; }
+input { display: block; width: 100%; margin-bottom: 10px; }
+</style>

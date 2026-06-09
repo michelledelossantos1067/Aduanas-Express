aee096c1e258973891b69b3cb407f4e3fad5d413
Carlos Reynoso <malacacatuana5@gmail.com>
2026-06-09 11:58:56 -0400

    Mapping de Asignacion

---
+++ AduanasExpress.Application/Mappings/AsignacionMapping.cs
@@ -0,0 +1,21 @@
+﻿using AduanasExpress.Application.DTOs.Asignacion;
+using AduanasExpress.Domain.Entitis;
+
+namespace AduanasExpress.Application.Mappings
+{
+    public static class AsignacionMapping
+    {
+        public static AsignacionResponseDTO ToResponse(this Asignacion asignacion)
+        {
+            return new AsignacionResponseDTO
+            {
+                Id = asignacion.Id,
+                SolicitudId = asignacion.SolicitudId,
+                VehiculoId = asignacion.VehiculoId,
+                ConductorId = asignacion.ConductorId,
+                FechaAsignacion = asignacion.FechaAsignacion,
+                AsignadoPorId = asignacion.AsignadoPorId
+            };
+        }
+    }
+}

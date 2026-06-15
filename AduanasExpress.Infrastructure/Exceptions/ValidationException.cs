92532c86b9273485d79dfb613a0f5307b85c7bc8
Carlos Reynoso <malacacatuana5@gmail.com>
2026-06-15 10:49:43 -0400

    feat: creacion de excepcion de validacion

---
+++ AduanasExpress.Infrastructure/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AduanasExpress.Infrastructure.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string mensaje): base mensaje { }
+    }
+}
